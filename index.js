d18117410f2d264f79159fb6fb5ed72deec9c9cc
Adrian Rivera <avrivera1@up.edu.ph>
2025-02-21 14:13:36 +0800

    feat: Added the addAccount function; chore: updated the test.js file with the new test cases.

--- index.js
+++ index.js
@@ -17,10 +17,33 @@ function generateUniqueID(fname, lname){
     return uniqueID; 
 }
 
+function addAccount(array_info){
 
+    if (!Array.isArray(array_info)) return false; // check if the input is an array
+    if (array_info.length < 4) return false; // check if the array has at least 4 elements
+    if (array_info.length > 4) { 
+        array_info = array_info.slice(0, 4); // if the array exceeds the 4 elements, only the first 4 elements will be considered
+    }
+
+    const [fname, lname, email, age] = array_info; // array destructuring
+
+    if (typeof fname !== 'string' || typeof lname !== 'string' || typeof email !== 'string' || !Number.isInteger(age)) 
+        return false; // check if the elements are strings
+
+    if (!validator.isEmail(email)) return false; // check if the email is valid
+    if (age < 18) return false; // check if the age is greater than or equal to 18
+
+    const uniqueID = generateUniqueID(fname, lname); // generate a unique ID using the function generateUniqueID
+    const userData = `${fname},${lname},${email},${age},${uniqueID}\n`; // format the user data that will be written to the file
+
+   
+    fs.appendFileSync('users.txt', userData); // append the user data to the file
+    return true; // only return true if all the conditions are met
+    
+}
 
 export { // export the functions
     generateUniqueID,
-    //addAccount
+    addAccount
 };
 
--- test.js
+++ test.js
@@ -7,7 +7,7 @@ console.log(generateUniqueID('Jane', 'Smith'));
 
 
 // Test cases for addAccount
-// console.log(addAccount(['John', 'Doe', 'jsajdad', 25])); // false
-// console.log(addAccount(['John', 'Doe', 'avrivera1@gmail.com', 25])); // true
-// console.log(addAccount(['Alice', 'Johnson', 'alice.johnson@example.com', 30])); // true
-// console.log(addAccount(['Hannah', 'Melo-Rivera', 'hannahmeloR@yahoo.com', 17])); // false
+console.log(addAccount(['John', 'Doe', 'jsajdad', 25])); // false
+console.log(addAccount(['John', 'Doe', 'avrivera1@gmail.com', 25])); // true
+console.log(addAccount(['Alice', 'Johnson', 'alice.johnson@example.com', 30])); // true
+console.log(addAccount(['Hannah', 'Melo-Rivera', 'hannahmeloR@yahoo.com', 17])); // false
