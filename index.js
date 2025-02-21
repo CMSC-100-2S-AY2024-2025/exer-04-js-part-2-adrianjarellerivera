192a60ab45e52f2fc8e7cc3c2c355bcea512d012
Adrian Rivera <avrivera1@up.edu.ph>
2025-02-21 13:39:45 +0800

    chore: Added documentation for the agenerateUniqueID function and added test cases on the test.js file

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 // Date: 2025/02/21
 // Exercise 4: Simulating a password validation program in JavaScript
 
-// Importing packages
+// Packages
 import { v4 as uuid } from 'uuid';
 import validator from 'validator';
 import fs from 'fs';
 
-
 function generateUniqueID(fname, lname){
-    if ( typeof fname !== 'string' || typeof lname !== 'string') 
+
+    if ( typeof fname !== 'string' || typeof lname !== 'string') // check if the input is a string
         return false;
 
-    const uniqueString = uuid().split('-').join('').slice(0, 8);
-    const uniqueID = fname[0].toLowerCase() + lname.toLowerCase() + uniqueString;
-    return uniqueID;
+    const uniqueString = uuid().split('-').join('').slice(0, 8); // generate a unique string using the uuid package
+    const uniqueID = fname[0].toLowerCase() + lname.toLowerCase() + uniqueString; // concatenate based on the format provided 
+
+    return uniqueID; 
 }
 
-console.log(generateUniqueID('John', 'Doe')); // jdoe3b4c5d
-console.log(generateUniqueID('Jane', 'Smith')); // jsmith3b4c5d
 
+
+export { // export the functions
+    generateUniqueID,
+    //addAccount
+};
 
---
+++ test.js
@@ -0,0 +1,13 @@
+import { generateUniqueID, addAccount } from './index.js';
+
+
+// Test cases for generateUniqueID
+console.log(generateUniqueID('John', 'Doe')); 
+console.log(generateUniqueID('Jane', 'Smith'));
+
+
+// Test cases for addAccount
+// console.log(addAccount(['John', 'Doe', 'jsajdad', 25])); // false
+// console.log(addAccount(['John', 'Doe', 'avrivera1@gmail.com', 25])); // true
+// console.log(addAccount(['Alice', 'Johnson', 'alice.johnson@example.com', 30])); // true
+// console.log(addAccount(['Hannah', 'Melo-Rivera', 'hannahmeloR@yahoo.com', 17])); // false
